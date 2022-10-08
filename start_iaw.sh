a25cc64b029843547f0294dd395f025b78467b7a
Tomas Pinerio <tomaspia95@gmail.com>
2022-10-08 10:46:13 +0200

    Aun no funciona bien star y stop no esta acabado

---
+++ start_iaw.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#echo #off
+
+/opt/lampp/lampp startapache
+
+code /opt/lampp/htdocs/tomas 
+
+spotify --uri=https://open.spotify.com/playlist/54bvft8RCYbNBkK0LQAuP4?si=5eb9ef4a1721422d 
+
+
+
+echo "& \n"
+
---
+++ stop_iaw.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#echo #off
+
+/opt/lampp/lampp stopapache
